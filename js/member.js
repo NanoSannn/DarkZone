2cd0830982d8b42e570e2c3fa7e0be43d0fcf46a
AnimeAbout <animeoutab@gmail.com>
2023-08-06 23:42:33 +0700

    remove code

--- js/member.js
+++ js/member.js
@@ -15,8 +15,6 @@ for (let i = 0; i <= memberCount; i++) {
   updateElement(`${memberId}_contact`, text.contact);
   updateElement(`${memberId}_info`, text.info);
   updateElement(`${memberId}_info_1`, currentMember.info1);
-  updateElement(`${memberId}_info_2`, currentMember.info2);
-  updateElement(`${memberId}_info_3`, currentMember.info3);
   updateElement(`${memberId}_detail`, currentMember.detail);
 
   const updateLink = (elementId, href, style) => {
